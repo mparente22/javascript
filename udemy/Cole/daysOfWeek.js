a05b2c2df006a745efb277c5d83fd800f0a86c20
mparente22 <michael.parente@busybeemedia.com>
2022-02-27 09:54:38 -0800

    Create daysOfWeek.js

---
+++ udemy/Cole/daysOfWeek.js
@@ -0,0 +1,32 @@
+const age = 100;
+if (age < 5){
+    console.log('FREE')
+} else if (age < 10){
+    console.log('$10')
+} else if (age < 65){
+    console.log('$20')
+} else if (age < 85){
+    console.log('$10')
+} else {
+    console.log('DAMN BRUH')
+}
+
+const dayOfWeek = prompt('ENTER A DAY').toLowerCase();
+
+if (dayOfWeek === 'monday'){
+    console.log('Monday SUCKS!');
+} else if (dayOfWeek === 'tuesday'){
+    console.log('Tuesday. Only good for TACOS!!!');
+} else if (dayOfWeek === 'wednesday'){
+    console.log('Wednesday is atleast almost to Friday!');
+} else if (dayOfWeek === 'thursday'){
+    console.log('Thursday...Did you mean Friday....LOL');
+} else if (dayOfWeek === 'friday'){
+    console.log('FUCK YES! FRIDAY IS HERE!!');
+} else if (dayOfWeek === 'saturday'){
+    console.log('Sleep in Saturday!!');
+} else if (dayOfWeek === 'sunday'){
+    console.log('Sunday. Tomorrow is Monday :-( ');
+} else {
+    console.log('ENTER A DAY!')
+}
